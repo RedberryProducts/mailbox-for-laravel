25fc0b543a9c7ff327f8c4a8fb9aa6923a31b53b
Nika Jorjoliani <nikajorjika1993@gmail.com>
2026-04-16 15:25:54 +0400

    fix: update asset file references in manifest and adjust title attribute in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title inertia>Mailbox for Laravel</title>
+    <title data-inertia>Mailbox for Laravel</title>
     
     {{ Vite::useHotFile('vendor/mailbox/mailbox.hot')
         ->useBuildDirectory("vendor/mailbox")
